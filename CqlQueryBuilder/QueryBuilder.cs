f0bd499ea505d2f5019ed160f74c8b27025f6e5c
Nelson Souza <njunior1980@gmail.com>
2018-11-13 20:44:32 +0000

    * Added new delete methods * Select aggregate * Rename IFilterCondition to IFilterExtension
    Necessary to create the aggregates :)
    
    SELECT name, max (points), COUNT (*)
    FROM users;

--- CqlQueryBuilder/QueryBuilder.cs
+++ CqlQueryBuilder/QueryBuilder.cs
@@ -15,8 +15,29 @@ namespace CqlQueryBuilder
         public SelectBuilder<T> Select<T>() where T : class => 
             new SelectBuilder<T>(QueryHelper.Select<T>());
 
-        public SelectBuilder<T> SelectCount<T>() where T : class =>
-            new SelectBuilder<T>(QueryHelper.SelectCount<T>());
+        public SelectBuilder<T> Aggregate<T>(Expression<Func<T, object>> firstParameter, 
+            Expression<Func<T, object>>[] parameters, bool isCount) where T : class
+        {
+            var p1 = QueryCreate.GetParameterName(firstParameter);
+            var p2 = string.Empty;
+
+            if (parameters != null)
+                p2 = QueryCreate.GetParameterName(parameters);
+
+            return isCount 
+                ? new SelectBuilder<T>(QueryHelper.SelectCount<T>(p1, p2)) 
+                : new SelectBuilder<T>(QueryHelper.SelectMax<T>(p1, p2));
+        }
+
+        public SelectBuilder<T> SelectCount<T>(Expression<Func<T, object>> countParameter, params Expression<Func<T, object>>[] parameters) where T : class
+        {
+            return Aggregate(countParameter, parameters, true);
+        }
+
+        public SelectBuilder<T> SelectMax<T>(Expression<Func<T, object>> maxParameter, params Expression<Func<T, object>>[] parameters) where T : class
+        {
+            return Aggregate(maxParameter, parameters, false);
+        }
 
         public SelectBuilder<T> Select<T>(params Expression<Func<T, object>>[] parameters) 
             where T : class => new SelectBuilder<T>(QueryHelper.Select<T>(QueryCreate.GetParameterName(parameters)));
